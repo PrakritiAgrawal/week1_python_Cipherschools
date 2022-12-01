c45155b28c14630e39bf0bf5e4e5f1ec1763cf79
PrakritiAgrawal <119535105+PrakritiAgrawal@users.noreply.github.com>
2022-12-01 22:52:36 +0530

    Create activity7.py

---
+++ activity7.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+age = int(age)
+if age >= 10 and (name[0]=="A" or name[0]=="a"):
+    print("you can watch coco")
+else:
+    print("sorry! you can't watch coco")
