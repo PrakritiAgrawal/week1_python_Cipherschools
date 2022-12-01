239535e9e1257399dcb2bf322665198da001475b
PrakritiAgrawal <119535105+PrakritiAgrawal@users.noreply.github.com>
2022-12-01 22:51:36 +0530

    Create activity6.py

---
+++ activity6.py
@@ -0,0 +1,12 @@
+wining_number = 3
+guess_number= int(input("Enter any number here between 1 to 10: "))
+if guess_number == wining_number:
+    print("you win !!")
+if guess_number> wining_number:
+    print("too high")
+if guess_number< wining_number:
+    print("too low")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
